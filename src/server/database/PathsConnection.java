55903694adcbe55b47eb95aceacaa0b20839ec6f
Rob <robert.systemdeveloper@gmail.com>
2020-12-08 15:07:10 +0100

    Add methods for getting all notes, paths and categories from db

--- src/server/database/PathsConnection.java
+++ src/server/database/PathsConnection.java
@@ -1,6 +1,14 @@
 package server.database;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import express.utils.Utils;
+import server.model.Path;
+
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
 
 public class PathsConnection {
 
@@ -8,5 +16,25 @@ public class PathsConnection {
 
     public PathsConnection(Connection dbConnection) {
         this.dbConnection = dbConnection;
+    }
+
+    public List<Path> getPaths(){
+
+        List<Path> paths = null;
+        String query = "SELECT * FROM paths";
+
+        try {
+            PreparedStatement preparedStatement = dbConnection.prepareStatement(query);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            Path [] pathsResultset = (Path[]) Utils.readResultSetToObject(resultSet,Path[].class);
+            paths = List.of(pathsResultset);
+
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+
+        return paths;
     }
 }
