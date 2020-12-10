690ce4061f5efcc72e40cb76a843709bc5895daa
Per Kristian <kristiansvanberg@gmail.com>
2020-12-10 11:59:23 +0100

    main.js
    Added a blur effect via jquery for when klicking on the sidebar filter button. Also added logic for handling the displaying of the lightbox (the image gallery).
    also added in commented code for querystrings

--- src/client/main.js
+++ src/client/main.js
@@ -8,12 +8,33 @@
   // opening and closing navbar:
   $("#open-navbar").click(function() {
     $("#side-navbar").css("width", "500px");
+    $(".container").addClass("blur");
     $(".navbar-wrapper").addClass("open");
   });
 
   $("#close-navbar").click(function() {
     $("#side-navbar").css("width", "0");
+    $(".container").removeClass("blur");
     $(".navbar-wrapper").removeClass("open");
   });
+
+  // event delegation, preventing redirecting to image, just open it in lightbox
+  $(document).on('click', '[data-toggle="lightbox"]', function(e) {
+    e.preventDefault();
+    $(this).ekkoLightbox();
+  });
+
+  // testing querystring
+  // -------------------
+  // function getParameterByName(name, url = window.location.href) {
+  //   name = name.replace(/[\[\]]/g, '\\$&');
+  //   let regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
+  //       results = regex.exec(url);
+  //   if (!results) return null;
+  //   if (!results[2]) return '';
+  //   return decodeURIComponent(results[2].replace(/\+/g, ' '));
+  // }
+  //
+  // console.log(getParameterByName("note-id"));
 
 })(jQuery);
