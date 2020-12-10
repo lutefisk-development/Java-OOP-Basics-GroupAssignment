0776ff8d4a75de7fe51d2a66ac2144ffd437735b
Dino Ferizovic <dinoferizovic@gmail.com>
2020-12-10 14:21:16 +0100

    Single Category
    Added EndPoint & Connection for single Category.

--- src/server/endpoints/CategoryEndpoints.java
+++ src/server/endpoints/CategoryEndpoints.java
@@ -15,11 +15,26 @@ public class CategoryEndpoints {
 
     private void categoryEndpoints(Database dbConnection, Express app){
 
+        // all categories
         app.get("/rest/categories", (((request, response) -> {
 
             List<Category> categories = dbConnection.getCategoriesConnection().getCategories();
             response.json(categories);
         })));
+
+        // category by id
+        app.get("/rest/categories/:id", (req, res) -> {
+            try {
+                int id = Integer.parseInt(req.getParam("id"));
+                Category category = dbConnection.getCategoriesConnection().getCategoryById(id);
+
+                res.json(category);
+
+            } catch (NumberFormatException exception) {
+                exception.printStackTrace();
+            }
+
+        });
 
     }
 }
