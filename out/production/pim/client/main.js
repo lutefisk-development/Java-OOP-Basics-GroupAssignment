48ff37990c38aa8f4a64bf2bf48836529119d897
Emil Lagerstedt <emil.lagerstedt@me.com>
2020-12-11 11:05:13 +0100

    produktion files
    pro

--- out/production/pim/client/main.js
+++ out/production/pim/client/main.js
@@ -8,12 +8,102 @@
   // opening and closing navbar:
   $("#open-navbar").click(function() {
     $("#side-navbar").css("width", "500px");
+    $(".container").addClass("blur");
     $(".navbar-wrapper").addClass("open");
   });
 
   $("#close-navbar").click(function() {
     $("#side-navbar").css("width", "0");
+    $(".container").removeClass("blur");
     $(".navbar-wrapper").removeClass("open");
   });
 
+  // event delegation, preventing redirecting to image, just open it in lightbox
+  $(document).on('click', '[data-toggle="lightbox"]', function(e) {
+    e.preventDefault();
+    $(this).ekkoLightbox();
+  });
+
+  // testing querystring
+  // -------------------
+  // function getParameterByName(name, url = window.location.href) {
+  //   name = name.replace(/[\[\]]/g, '\\$&');
+  //   let regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
+  //       results = regex.exec(url);
+  //   if (!results) return null;
+  //   if (!results[2]) return '';
+  //   return decodeURIComponent(results[2].replace(/\+/g, ' '));
+  // }
+  //
+  // console.log(getParameterByName("note-id"));
+
+
+
+
+
+
+
+
+
+
+
+
+
+  // Async functions for PathEndpoints
+
+  async function getPathsFromDb(){
+
+    let result = await fetch("/rest/paths");
+    let paths = await result.json();
+    console.log(paths);
+
+  }
+
+  async function createPathInDb(path){
+
+    let result = await fetch("/rest/paths", {
+        method: "POST",
+        body: JSON.stringify(path)
+    });
+
+    console.log(await result.text());
+  }
+
+  async function deletePathInDb(path){
+
+    let result = await fetch("/rest/paths/id", {
+        method: "DELETE",
+        body: JSON.stringify(path)
+    });
+
+
+    console.log(await result.text());
+
+  }
+
+  // la till en get all notes function för att spara i en array for att delete ska funka, kan ha fel!
+  let notes = [];
+
+  async function getAllNotes(){
+    let result = await fetch("/rest/notes");
+    notes = await result.json();
+  }
+
+  async function getNote(){
+    let result = await fetch("/rest/notes/id");
+    notes = await result.json();
+  }
+
+  $("#deleteNoteByIdButton").click(function() {
+    deleteNoteById();
+
+  });
+
+  async function deleteNoteById(id){
+    let result = await fetch("/rest/notes/id", {
+      method: "DELETE",
+      body: JSON.stringify(id)
+    });
+    
+  }
 })(jQuery);
