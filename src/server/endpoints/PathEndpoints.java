6a615a5467bc102121f4832391312f395947c90b
Per Kristian <kristiansvanberg@gmail.com>
2020-12-14 15:24:25 +0100

    PathEndpoints
    Changed post request in PathEndpoints

--- src/server/endpoints/PathEndpoints.java
+++ src/server/endpoints/PathEndpoints.java
@@ -36,18 +36,32 @@ public class PathEndpoints {
             res.send(imgUrl);
         });
 
-        /*
+
         app.get("/rest/paths",(((request, response) -> {
 
             List<Path> paths = dbConnection.getPathsConnection().getPaths();
             response.json(paths);
         })));
 
-        app.post("/rest/paths", (((request, response) -> {
-            Path path = (Path)request.getBody(Path.class);
-            dbConnection.getPathsConnection().createPath(path);
-            response.send("Post went ok.");
-        })));
+        app.post("/rest/paths", (request, response) -> {
+            Path newPath = (Path)request.getBody(Path.class);
+            int id = dbConnection.getPathsConnection().createPath(newPath);
+
+            if(id != 0) {
+                newPath.setId(id);
+
+                Path path = dbConnection.getPathsConnection().getPathById(id);
+
+                if(path == null) {
+                    System.out.println("There is no record of this path");
+                } else {
+                    System.out.println(path.toString());
+                    response.send("Successfully created a new path with id: " + id);
+                }
+            } else {
+                System.out.println("Something went wrong");
+            }
+        });
 
         app.delete("/rest/paths/:id", (((request, response) -> {
 
@@ -58,7 +72,7 @@ public class PathEndpoints {
                 response.send("Delete went ok.");
             }
         })));
-         */
+
 
 
     }
