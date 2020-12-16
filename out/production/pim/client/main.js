4cca5742a01dd588d82016b00bb45e328767df6a
Per Kristian <kristiansvanberg@gmail.com>
2020-12-16 09:42:28 +0100

    Prod files

--- out/production/pim/client/main.js
+++ out/production/pim/client/main.js
@@ -32,25 +32,29 @@
     handleFormSubmit();
   });
 
-  // update note form:
-  $("#update-note-form").submit(function(e) {
-
-    // stop form from submitting.
-    e.preventDefault();
-
-    // handling the form data
-    //handleFormSubmit();
-  })
-
   const handleFormSubmit = async () => {
     // getting file if user has added one
     let fileUrl;
+    let fileType = "";
     if($("#note-file").prop('files').length > 0) {
       let $fileArray = $("#note-file").prop('files');
+
+      console.log($fileArray);
+
       let formData = new FormData();
 
       // adding the file to formData
       for(let file of $fileArray) {
+
+        // if it's a png or jpeg file, set variable to "img"
+        if(file.type.split("/")[1] == "png" || file.type.split("/")[1] == "jpeg") {
+          fileType = "img"
+        }
+
+        // if it's pdf or txt file, set variable to "file"
+        if(file.type.split("/")[1] == "pdf" || file.type.split("/")[1] == "plain") {
+          fileType = "file"
+        }
         formData.append('files', file, file.name);
       }
 
@@ -92,7 +96,7 @@
     let newPath = {
       path: fileUrl ? fileUrl : null,
       noteId: newNoteFromDb.id,
-      fileType: "img"
+      fileType: fileType
     }
 
     //only make a new path in db if the user actually has inserted a file
@@ -267,11 +271,12 @@
       cat = '<option value =' + '"' + categories[i].id + '"' + '>' + categories[i].category + '</option>' ;
       categoryList.innerHTML += cat;
     }
+  }
 
+  $(document).ready(function() {
+    $(document).on('submit', '#update-note-form', async function(e) {
 
-
-    $("#update-button").click(async function(){
-
+      e.preventDefault();
       let id = currentUrl.split("note-id=")[1];
       let note = await getNoteById(id);
 
@@ -286,42 +291,92 @@
         finishDate: $("#note-end").val()
       }
 
-      updateNoteInDb(updatedNote);
+      await updateNoteInDb(updatedNote);
+      await addFileToNote(id);
       window.location.replace("http://localhost:1000/");
     });
+  });
 
+  const addFileToNote = async id => {
+    // getting file if user has added one
+    let fileUrl;
+    let fileType = "";
+    if($("#note-file").prop('files').length > 0) {
+      let $fileArray = $("#note-file").prop('files');
+
+      console.log($fileArray);
+
+      let formData = new FormData();
+
+      // adding the file to formData
+      for(let file of $fileArray) {
+
+        // if it's a png or jpeg file, set variable to "img"
+        if(file.type.split("/")[1] == "png" || file.type.split("/")[1] == "jpeg") {
+          fileType = "img"
+        }
+
+        // if it's pdf or txt file, set variable to "file"
+        if(file.type.split("/")[1] == "pdf" || file.type.split("/")[1] == "plain") {
+          fileType = "file"
+        }
+        formData.append('files', file, file.name);
+      }
+
+      // sending post request to endpoint for storing the file
+      let uploadResult = await fetch('/rest/file-upload', {
+        method: 'POST',
+        body: formData
+      });
+
+      // get path
+      fileUrl = await uploadResult.text();
+    }
+
+    // setting path variable
+    let newPath = {
+      path: fileUrl ? fileUrl : null,
+      noteId: id,
+      fileType: fileType
+    }
+
+    //only make a new path in db if the user actually has inserted a file
+    if(newPath.path != null) {
+      let pathResult = await fetch("/rest/paths", {
+        method: "POST",
+        body: JSON.stringify(newPath),
+      });
+    }
   }
 
-    // show single note by id
-    if(currentUrl.includes("/single_note.html?note-id=")) {
-      let id = currentUrl.split("=")[1];
-      showSingleNoteById(id);
-    }
+  // show single note by id
+  if(currentUrl.includes("/single_note.html?note-id=")) {
+    let id = currentUrl.split("=")[1];
+    showSingleNoteById(id);
+  };
 
-    async function showSingleNoteById(id) {
-      let note = await getNoteById(id);
-      let paths = await getPathsFromDb(id);
+  async function showSingleNoteById(id) {
+    let note = await getNoteById(id);
+    let paths = await getPathsFromDb(id);
 
-          // checks if there is a end date, if not set default message
+    // checks if there is a end date, if not set default message
     if(note.finishDate == "") {
       note.finishDate = "No date set"
-    }
+    };
 
     let imgs = [];
     let files = [];
 
     // loops through paths and divids up files and images into other arrays
     for(let i = 0; i < paths.length; i++) {
-
       if(paths[i].fileType == "img") {
         imgs.push(paths[i]);
       } else {
         files.push(paths[i]);
-      }
+      };
+    };
 
-    }
-
-    if(imgs.length > 0) {
+    if(imgs.length > 0 && files.length > 0) {
 
       $("#single-note").append(
         '<div class="section-header">' +
@@ -344,10 +399,12 @@
           '<h2>'+ note.title +'</h2>' +
           '<p>'+ note.text +'</p>' +
         '</div>' +
-        '<div class="section-images"></div>'
+        '<div class="section-images"></div>' +
+        '<div class="section-files"></div>'
       );
 
-    } else if(files.length > 0) {
+    } else {
+
       $("#single-note").append(
         '<div class="section-header">' +
           '<div class="dates">' +
@@ -371,62 +428,33 @@
         '</div>' +
         '<div class="section-files"></div>'
       );
-    
-    } else {
+    };
 
-      $("#single-note").append(
-        '<div class="section-header">' +
-          '<div class="dates">' +
-            '<div class="created-date">' +
-              '<p>Created:</p>' +
-              '<p>'+ note.creationDate +'</p>' +
-            '</div>' +
-            '<div class="end-date">' +
-              '<p>Ends:</p>' +
-              '<p>'+ note.finishDate +'</p>' +
-            '</div>' +
-          '</div>'+
-          '<div class="edit-delete">' +
-            '<a href="/update_note.html?note-id='+ note.id +'" class="far fa-edit fa-2x"></a>' +
-            '<i class="far fa-trash-alt fa-2x"></i>' +
-          '</div>' +
-        '</div>'+
-        '<div class="section-body">' +
-          '<h2>'+ note.title +'</h2>' +
-          '<p>'+ note.text +'</p>' +
-        '</div>'
-      );
-    
-    }
-
-      // append imgages to .section-images
-      if(imgs.length > 0) {
-        for(let i = 0; i < imgs.length; i++) {
-          $(".section-images").append(
-            '<figure class="img-wrapper" id="img-'+ imgs[i].id +'">' +
-              '<a href="'+ imgs[i].path +'" data-toggle="lightbox">' +
-                '<img src="'+ imgs[i].path +'" alt="">' +
-              '</a>' +
-            '</figure>'
-          );
-        };
+    // append imgages to .section-images
+    if(imgs.length > 0) {
+      for(let i = 0; i < imgs.length; i++) {
+        $(".section-images").append(
+          '<figure class="img-wrapper" id="img-'+ imgs[i].id +'">' +
+            '<a href="'+ imgs[i].path +'" data-toggle="lightbox">' +
+              '<img src="'+ imgs[i].path +'" alt="">' +
+            '</a>' +
+          '</figure>'
+        );
       };
-  
-      // append files to .section-files
-      if(files.length > 0) {
-        for(let i = 0; i < files.length; i++) {
-          $(".section-files").append(
-            '<div class="file-container" id="file-'+ files[i].id +'">' +
-              '<i class="far fa-file-alt fa-3x"></i>' +
-              '<div>'+ files[i].path +'</div>' +
-            '</div>'
-          );
-        };
+    };
+
+    // append files to .section-files
+    if(files.length > 0) {
+      for(let i = 0; i < files.length; i++) {
+        $(".section-files").append(
+          '<div class="file-container" id="file-'+ files[i].id +'">' +
+            '<i class="far fa-file-alt fa-3x"></i>' +
+            '<a href="'+ files[i].path +'">'+ files[i].path.split("/")[2] +'</a>' +
+          '</div>'
+        );
       };
-
-
-
-    }
+    };
+  };
 
 
   async function getPathsFromDb(id){
@@ -434,7 +462,7 @@
     let result = await fetch("/rest/paths/" +id);
     let paths = await result.json();
     console.log(paths);
-    
+
     return paths;
   }
 
